f323465d1128febcb4361e81c513479141b25329
Vlad <vladst6001@users.noreply.github.com>
2026-06-25 11:37:49 +0300

    Remove Telegram SDK from main.tsx to fix grey screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
 
-import('@telegram-apps/sdk').then((sdk) => {
-  try { sdk.init(); } catch {}
-  try { sdk.backButton.mount(); } catch {}
-}).catch(() => {});
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
